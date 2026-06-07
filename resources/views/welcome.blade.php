b64f80060d9d96546027f67b9d0c8a78a1815741
Mrbaharu <juliantoking555@gmail.com>
2026-06-08 04:46:51 +0700

    update welcome"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,12 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Maen laga laga yo </h1>
+    <h1>for what is a man what has he got?<br>
+If not himself then he has naught<br>
+To say the things he truly feels
+And not the words of one who kneels
+<br>The record shows I took the blows<br>
+And did it my way
+    </h1>
 </body>
 </html>
